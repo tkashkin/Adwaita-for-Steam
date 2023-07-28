e7122f86a1f2d8966dda72448d97ad17ac156c33
Foldex <foldex@pm.me>
2023-07-27 23:16:04 -0400

    Update Installer

--- install.py
+++ install.py
@@ -4,8 +4,6 @@ from pathlib import Path
 from sys import platform
 from tempfile import TemporaryDirectory, NamedTemporaryFile
 from typing import NoReturn
-import configparser
-import re
 import shutil
 import time
 import os
@@ -41,12 +39,11 @@ else:
 	TEXT_CROSS = "✖"
 	TEXT_INFO = "✦"
 
-COLOR_THEME_DIR = "colorthemes"
+ADWAITA_DIR = "adwaita"
+COLOR_THEME_DIR = f"{ADWAITA_DIR}/colorthemes"
+CUSTOM_DIR = "custom"
 FONTS_DIR = "fonts"
-WEB_EXTRAS_DIR = "web/extras"
-WEB_THEME_DIR = "web"
-
-LIBRARY_CSS_FILE = "libraryroot.custom.css"
+WEB_EXTRAS_DIR = f"{ADWAITA_DIR}/extras"
 
 TARGET_NORMAL = "~/.steam/steam"
 TARGET_FLATPAK = "~/.var/app/com.valvesoftware.Steam/.steam/steam"
@@ -56,91 +53,81 @@ TARGET_FONTS = "~/.local/share/fonts"
 TARGET_FONTS_WIN = "AppData/Local/Microsoft/Windows/Fonts"
 
 STEAM_LOOPBACK = "https://steamloopback.host"
+STEAM_LOOPBACK_ADWAITA = f"{STEAM_LOOPBACK}/{ADWAITA_DIR}"
 STEAM_PATCHED_HEADER = "/*patched*/"
 
-STEAM_FRIENDS_CSS = "clientui/css/friends.css"
-STEAM_LIBRARY_CSS = "steamui/css/library.css"
-STEAM_ORIG_LIBRARY = "steamui/css/library.original.css"
-STEAM_ORIG_FRIENDS = "clientui/css/friends.original.css"
-STEAM_CUSTOM_LIBRARY = "steamui/libraryroot.custom.css"
-STEAM_CUSTOM_FRIENDS = "clientui/friends.custom.css"
+STEAM_UI_DIR = "steamui"
+STEAM_ADWAITA_DIR = f"{STEAM_UI_DIR}/{ADWAITA_DIR}"
+STEAM_CSS_DIR = f"{STEAM_UI_DIR}/css"
 
+STEAM_LIBRARY_CSS = f"{STEAM_CSS_DIR}/library.css"
+STEAM_ORIG_LIBRARY = f"{STEAM_CSS_DIR}/library.original.css"
+STEAM_CUSTOM_LIBRARY = f"{STEAM_UI_DIR}/libraryroot.custom.css"
+
+LIBRARY_ROOT_CSS = "libraryroot.custom.css"
+CUSTOM_CSS = "custom.css"
+
+
+adwaitadir = Path(ADWAITA_DIR)
 colorthemedir = Path(COLOR_THEME_DIR)
+customdir = Path(CUSTOM_DIR)
 fontdir = Path(FONTS_DIR)
 webextrasdir = Path(WEB_EXTRAS_DIR)
-webthemedir = Path(WEB_THEME_DIR)
 
-# CSS for Steam Library Patching
-LIBRARY_BASE_FILES = [
-	# Web
-	webthemedir / "base/_root.css",
-	webthemedir / "base/_localization.css",
-	webthemedir / "base/scrollbars.css",
+# CSS List for @import
+LIBRARY_FILES = [
+	# General
+	"variants/base/_root.css",
+	"variants/base/_localization.css",
+	"variants/base/scrollbars.css",
 	# Main window
-	webthemedir / "base/main_window/headerbar/headerbar.css",
-	webthemedir / "base/main_window/headerbar/buttons.css",
-	webthemedir / "base/main_window/headerbar/navigation.css",
-	webthemedir / "base/main_window/headerbar/menu.css",
-	# In-Game Overlay
-	webthemedir / "base/game_overlay.css",
-	# Notifications
-	webthemedir / "base/notifications.css",
+	"variants/base/main_window/headerbar/headerbar.css",
+	"variants/base/main_window/headerbar/buttons.css",
+	"variants/base/main_window/headerbar/navigation.css",
+	"variants/base/main_window/headerbar/menu.css",
 	# Library
-	webthemedir / "base/library.css",
-	webthemedir / "base/game_details.css",
-	webthemedir / "base/downloads.css",
-	webthemedir / "base/collections.css",
-	# Web Dialogs
-	webthemedir / "base/dialogs/_dialogs.css",
-	# Library Dialogs
-	webthemedir / "base/dialogs/login.css",
-	webthemedir / "base/dialogs/launch_options.css",
+	"variants/base/library.css",
+	"variants/full/library.css",
+	"variants/base/game_details.css",
+	"variants/full/game_details.css",
+	"variants/base/downloads.css",
+	"variants/full/downloads.css",
+	"variants/base/collections.css",
+	# Dialogs
+	"variants/base/dialogs/_dialogs.css",
+	"variants/base/dialogs/launch_options.css",
+	"variants/base/dialogs/login.css",
+	"variants/full/dialogs/_dialogs.css",
+	"variants/full/dialogs/about_steam.css",
+	"variants/full/dialogs/add_game.css",
+	"variants/full/dialogs/app_properties.css",
+	"variants/full/dialogs/content_management.css",
+	"variants/full/dialogs/install_game.css",
+	"variants/full/dialogs/paged_settings.css",
+	"variants/full/dialogs/product_activation.css",
+	"variants/full/dialogs/recent_players.css",
+	"variants/full/dialogs/screenshots.css",
+	"variants/full/dialogs/server_browser.css",
+	"variants/full/dialogs/steam_settings.css",
+	"variants/full/dialogs/system_information.css",
+	"variants/full/dialogs/uninstall.css",
+	"variants/full/dialogs/whats_new.css",
+	# In-Game Overlay
+	"variants/base/game_overlay.css",
+	# Notifications
+	"variants/base/notifications.css",
+	# Chat
+	"variants/full/chat.css",
 ]
 
-LIBRARY_FULL_FILES = [
-	# Web
-	webthemedir / "base/_root.css",
-	webthemedir / "base/_localization.css",
-	webthemedir / "base/scrollbars.css",
-	webthemedir / "full/chat.css",
-	# Main window
-	webthemedir / "base/main_window/headerbar/headerbar.css",
-	webthemedir / "base/main_window/headerbar/buttons.css",
-	webthemedir / "base/main_window/headerbar/navigation.css",
-	webthemedir / "base/main_window/headerbar/menu.css",
-	# In-Game Overlay
-	webthemedir / "base/game_overlay.css",
-	# Notifications
-	webthemedir / "base/notifications.css",
-	# Library
-	webthemedir / "base/library.css",
-	webthemedir / "full/library.css",
-	webthemedir / "base/game_details.css",
-	webthemedir / "full/game_details.css",
-	webthemedir / "base/downloads.css",
-	webthemedir / "full/downloads.css",
-	webthemedir / "base/collections.css",
-	# Web Dialogs
-	webthemedir / "base/dialogs/_dialogs.css",
-	webthemedir / "full/dialogs/_dialogs.css",
-	webthemedir / "full/dialogs/paged_settings.css",
-	# Library Dialogs
-	webthemedir / "base/dialogs/launch_options.css",
-	webthemedir / "base/dialogs/login.css",
-	webthemedir / "full/dialogs/about_steam.css",
-	webthemedir / "full/dialogs/add_game.css",
-	webthemedir / "full/dialogs/app_properties.css",
-	webthemedir / "full/dialogs/content_management.css",
-	webthemedir / "full/dialogs/install_game.css",
-	webthemedir / "full/dialogs/product_activation.css",
-	webthemedir / "full/dialogs/recent_players.css",
-	webthemedir / "full/dialogs/screenshots.css",
-	webthemedir / "full/dialogs/server_browser.css",
-	webthemedir / "full/dialogs/steam_settings.css",
-	webthemedir / "full/dialogs/system_information.css",
-	webthemedir / "full/dialogs/uninstall.css",
-	webthemedir / "full/dialogs/whats_new.css",
-]
+
+# Utils
+def copy_dir(source: Path, target: Path):
+	if not source.is_dir():
+		raise SystemExit(f"{TEXT_RED}{TEXT_CROSS} copy_dir:{source} is not a valid directory.{TEXT_RESET}")
+	if target.is_dir():
+		shutil.rmtree(target)
+	shutil.copytree(source, target)
 
 # List Options
 def list_options(type: str, options: list[Path], suffix: str, sourcedir: Path, arg: str):
@@ -193,99 +180,58 @@ def win_reg_font(name: str, target: Path):
 	winreg.SetValueEx(key, str(name), 0, winreg.REG_SZ, str(target))
 	winreg.CloseKey(key)
 
-# Webkit CSS
+# CSS Theme
 def find_web_extras() -> list[Path]:
 	return list(webextrasdir.glob("**/*.css"))
 
-def gen_webkit_theme(target: Path, name: str, selected_extras: list[Path]):
-	if name == "none":
-		return
-	elif name == "library_base":
-		selected_files = LIBRARY_BASE_FILES
-	elif name == "library_full":
-		selected_files = LIBRARY_FULL_FILES
-	else:
-		raise SystemExit(f"{TEXT_RED}{TEXT_CROSS} Invalid web theme selected: {name}{TEXT_RESET}")
+def format_import(prefix: str, string: str) -> str:
+	return f"@import url(\"{prefix}/{string}\");\n"
 
-	with open(target,'wb') as wfd:
-		for f in selected_files:
-			with open(f,'rb') as fd:
-				shutil.copyfileobj(fd, wfd)
+def generate_libraryroot(target: Path, selected_extras: list[Path], selected_theme: str, custom_css: bool):
+	content = "/* Main Files */\n"
+	for f in LIBRARY_FILES:
+		content += format_import(STEAM_LOOPBACK_ADWAITA, f)
 
-		if selected_extras:
-			print()
-			for f in selected_extras:
-				we = f.removesuffix(".css")
-				f = Path(f)
+	if selected_extras:
+		print()
+		content += "\n/* Extras */\n"
 
-				if not f.exists() or f.suffix != ".css":
-					f = webthemedir / "extras" / "{}{}".format(we, ".css")
+		for extra in selected_extras:
+			we = extra.removesuffix(".css")
+			test = webextrasdir / we
+			if test.with_suffix(".css").exists():
+				print(f"{TEXT_BLUE}{TEXT_ARROW} Applying web_extra {TEXT_BOLD}{we}{TEXT_RESET}{TEXT_BLUE}...{TEXT_RESET}")
+				content += format_import(STEAM_LOOPBACK_ADWAITA, f"extras/{we}.css")
+			else:
+				print(f"{TEXT_PURPLE}{TEXT_INFO} Web Extra: {TEXT_BOLD}{we}{TEXT_RESET}{TEXT_PURPLE} not found!{TEXT_RESET}")
+		print()
 
-				if f.exists():
-					with open(f,'rb') as fd:
-						print(f"{TEXT_BLUE}{TEXT_ARROW} Applying web_extra {TEXT_BOLD}{we}{TEXT_RESET}{TEXT_BLUE}...{TEXT_RESET}")
-						shutil.copyfileobj(fd, wfd)
-				else:
-					print(f"{TEXT_PURPLE}{TEXT_INFO} Web Extra: {TEXT_BOLD}{f}{TEXT_RESET}{TEXT_PURPLE} not found!{TEXT_RESET}")
-			print()
+	if selected_theme:
+		print(f"{TEXT_BLUE}{TEXT_ARROW} Applying color theme {TEXT_BOLD}{selected_theme}{TEXT_RESET}{TEXT_BLUE}...{TEXT_RESET}\n")
+		content += "\n/* ColorTheme */\n"
+		content += format_import(STEAM_LOOPBACK_ADWAITA, f"colorthemes/{selected_theme}/{selected_theme}.css")
+
+	if custom_css:
+		print(f"{TEXT_BLUE}{TEXT_ARROW} Applying custom css...{TEXT_RESET}\n")
+		content += "\n/* Custom CSS */\n"
+		content += format_import(STEAM_LOOPBACK_ADWAITA, f"custom/{CUSTOM_CSS}")
+
+	target.open('w').write(content)
 
 # Color Themes
 def find_color_themes() -> list[Path]:
-	return list(sorted(colorthemedir.glob("**/*.theme")))
+	return list(sorted(colorthemedir.glob("**/*.css")))
 
-def hex2css(name: str, hex: str) -> str:
-	return f'--{name}: {hex};'
-
-def replace_css_colors(target: Path, config: configparser.ConfigParser):
-	if args.uninstall:
-		return
-
-	with open (target, 'r' ) as f:
-		content = f.read()
-
-	for section in config.sections():
-		for (key, val) in config.items(section):
-			s = hex2css(key, val)
-			content = re.sub(f'^(\s+)--{key}: [^;]+;$', r'\1' + s, content, flags = re.M)
-
-	with open (target, 'w' ) as f:
-		f.write(content)
-
-# Install
-def install(source: Path, target: Path, name: str):
-	if target.is_dir():
-		if target.stem != "skins":
-			target = target / "skins"
-			target.mkdir(exist_ok = True)
-	else:
-		print(f"{TEXT_PURPLE}{TEXT_INFO} Directory {TEXT_BOLD}{target}{TEXT_RESET}{TEXT_PURPLE} does not exist{TEXT_RESET}")
-		return
-	print(f"{TEXT_BLUE}{TEXT_ARROW} Installing skin {TEXT_BOLD}{name}{TEXT_RESET}{TEXT_BLUE} into {TEXT_BOLD}{target}{TEXT_RESET}{TEXT_BLUE}...{TEXT_RESET}")
-	target_skin = target / name
-	if target_skin.exists():
-		shutil.rmtree(target_skin)
-	shutil.copytree(source, target_skin)
-
+# Patching
 def patch_client_css(source: Path, target: Path, name: str):
-	if not target.is_dir():
-		print(f"{TEXT_PURPLE}{TEXT_INFO} Directory {TEXT_BOLD}{target}{TEXT_RESET}{TEXT_PURPLE} does not exist{TEXT_RESET}")
-		return
-
 	print(f"{TEXT_BLUE}{TEXT_ARROW} Patching Steam Client {TEXT_BOLD}{name}{TEXT_RESET}{TEXT_BLUE} Files...{TEXT_RESET}")
-
 
 	if name == "Library":
 		target_css = target / STEAM_LIBRARY_CSS
 		orig_css = target / STEAM_ORIG_LIBRARY
-		custom_css = target / STEAM_CUSTOM_LIBRARY
-		custom_css_name = custom_css.name
-		source_css = source / LIBRARY_CSS_FILE
-	# elif name == "Friends":
-	# 	target_css = target / STEAM_FRIENDS_CSS
-	# 	orig_css = target / STEAM_ORIG_FRIENDS
-	# 	custom_css = target / STEAM_CUSTOM_FRIENDS
-	# 	custom_css_name = custom_css.name
-	# 	source_css = source / CSS_FILE
+		custom_library = target / STEAM_CUSTOM_LIBRARY
+		custom_library_name = custom_library.name
+		source_css = source / LIBRARY_ROOT_CSS
 	else:
 		raise SystemExit(f"{TEXT_RED}{TEXT_CROSS} Invalid steam css patch selected: {name}{TEXT_RESET}")
 
@@ -295,10 +241,11 @@ def patch_client_css(source: Path, target: Path, name: str):
 
 	if args.uninstall:
 		print(f"{TEXT_BLUE}{TEXT_ARROW} Uninstalling, resetting patched Steam {name} CSS...{TEXT_RESET}")
-		open(custom_css, 'w').close()
+		open(custom_library, 'w').close()
 	else:
-		shutil.copy(source_css, custom_css)
+		shutil.move(source_css, custom_library)
 
+	# Skip if already patched
 	with target_css.open() as css_file:
 		if css_file.readline().strip() == STEAM_PATCHED_HEADER:
 			return
@@ -306,7 +253,11 @@ def patch_client_css(source: Path, target: Path, name: str):
 	orig_css = target_css.rename(target_css.with_suffix(".original.css"))
 	name = target_css.stem
 	css_dir = "css"
-	content = f'{STEAM_PATCHED_HEADER}\n@import url("{STEAM_LOOPBACK}/{css_dir}/{name}.original.css");\n@import url("{STEAM_LOOPBACK}/{custom_css_name}");\n'
+
+	content = f'{STEAM_PATCHED_HEADER}\n'
+	content += format_import(STEAM_LOOPBACK, f"{css_dir}/{name}.original.css")
+	content += format_import(STEAM_LOOPBACK, f"{custom_library_name}")
+
 	target_css.open('w').write(content)
 
 	size_diff = orig_css.stat().st_size - target_css.stat().st_size
@@ -331,19 +282,20 @@ def dev_reload(target: Path):
 
 # Run
 if __name__ == "__main__":
-	if not webthemedir.exists():
-		raise SystemExit(f"{TEXT_RED}{TEXT_CROSS} Web Theme directory {TEXT_BOLD}{WEB_THEME_DIR}{TEXT_RESET}{TEXT_RED} does not exist. Make sure you're running the installer from its root directory{TEXT_RESET}")
+	if not adwaitadir.exists():
+		raise SystemExit(f"{TEXT_RED}{TEXT_CROSS} Web Theme directory {TEXT_BOLD}{ADWAITA_DIR}{TEXT_RESET}{TEXT_RED} does not exist. Make sure you're running the installer from its root directory{TEXT_RESET}")
 	if not colorthemedir.exists():
 		raise SystemExit(f"{TEXT_RED}{TEXT_CROSS} Color Theme directory {TEXT_BOLD}{COLOR_THEME_DIR}{TEXT_RESET}{TEXT_RED} does not exist. Make sure you're running the installer from its root directory{TEXT_RESET}")
 
 	parser = ArgumentParser(description = "Adwaita-for-Steam installer")
 	parser.add_argument("-c", "--color-theme", default = "adwaita", help = "Choose color theme")
+	parser.add_argument("-cu", "--custom-css", action = "store_true", help = "Enable Custom CSS")
 	parser.add_argument("-d", "--dev", action = "store_true", help = "Dev Mode")
 	parser.add_argument("-fi", "--font-install", action = "store_true", help = "Install Fonts")
 	parser.add_argument("-l", "--list-options", action = "store_true", help = "List available themes & web extras and exit")
 	parser.add_argument("-t", "--target", nargs = "+", action = "extend", default = ["normal", "flatpak"], help = "Install targets: 'normal', 'flatpak', custom paths")
 	parser.add_argument("-u", "--uninstall", action = "store_true", help = "Uninstall theme")
-	parser.add_argument("-w", "--web-theme", choices = ["base", "full"], default = "full", help = "Choose web theme variant")
+	parser.add_argument("-w", "--web-theme", choices = ["base", "full"], default = "full", help = "Deprecated, does nothing.") # Keeping for compatability
 	parser.add_argument("-we", "--web-extras", nargs = "+", action = "extend", help = "Enable one or multiple web theme extras")
 	args = parser.parse_args()
 
@@ -351,7 +303,7 @@ if __name__ == "__main__":
 		args.target = ["windows"]
 
 	if args.list_options:
-		list_options("color themes", find_color_themes(), ".theme", colorthemedir, "color-theme")
+		list_options("color themes", find_color_themes(), ".css", colorthemedir, "color-theme")
 		list_options("web extras", find_web_extras(), ".css", webextrasdir, "web-extras")
 		exit(0)
 
@@ -360,33 +312,27 @@ if __name__ == "__main__":
 
 	selected_theme = None
 	if args.color_theme:
-		ct = Path(args.color_theme)
 
-		if args.color_theme.removesuffix(".theme") == "adwaita":
+		if args.color_theme == "adwaita":
 			selected_theme = None
-		elif ct.exists() and ct.suffix == ".theme":
-			selected_theme = ct
 		else:
-			t = args.color_theme.removesuffix(".theme")
-			selected_theme = colorthemedir / t / "{}{}".format(t, ".theme")
-			if not selected_theme.exists():
-				raise SystemExit(f"{TEXT_RED}{TEXT_CROSS} {TEXT_BOLD}{selected_theme}{TEXT_RESET}{TEXT_RED} theme not found.{TEXT_RESET}")
+			t = args.color_theme.removesuffix(".css")
+			selected_theme = t
+			test = colorthemedir / t / t
+			if not test.with_suffix(".css").exists():
+				raise SystemExit(f"{TEXT_RED}{TEXT_CROSS} {TEXT_BOLD}{test}{TEXT_RESET}{TEXT_RED} theme not found.{TEXT_RESET}")
+
 
 	with TemporaryDirectory() as tmpdir:
 		tmp = Path(tmpdir)
-		sourcedir = tmp
+
+		copy_dir(adwaitadir, tmp / ADWAITA_DIR)
+		copy_dir(customdir, tmp / ADWAITA_DIR / CUSTOM_DIR)
+		sourcedir = tmp / ADWAITA_DIR
+
 		print(f"{TEXT_BLUE}{TEXT_ARROW} Creating stage directory {TEXT_BOLD}{sourcedir}{TEXT_RESET}")
 
-		if args.web_theme == "full":
-			gen_webkit_theme(sourcedir / LIBRARY_CSS_FILE, "library_full", args.web_extras)
-		elif args.web_theme == "base":
-			gen_webkit_theme(sourcedir / LIBRARY_CSS_FILE, "library_base", args.web_extras)
-
-		if selected_theme:
-			print(f"{TEXT_BLUE}{TEXT_ARROW} Applying color theme {TEXT_BOLD}{selected_theme}{TEXT_RESET}{TEXT_BLUE}...{TEXT_RESET}")
-			config = configparser.ConfigParser()
-			config.read(selected_theme)
-			replace_css_colors(sourcedir / LIBRARY_CSS_FILE, config)
+		generate_libraryroot(sourcedir / LIBRARY_ROOT_CSS, args.web_extras, selected_theme, args.custom_css)
 
 		targets = set()
 
@@ -401,7 +347,12 @@ if __name__ == "__main__":
 				targets.add(Path(t).expanduser().resolve())
 
 		for target in targets:
+			if not target.is_dir():
+				print(f"{TEXT_PURPLE}{TEXT_INFO} Directory {TEXT_BOLD}{target}{TEXT_RESET}{TEXT_PURPLE} does not exist{TEXT_RESET}")
+				continue
+
 			patch_client_css(sourcedir, target, "Library")
+			copy_dir(sourcedir, target / STEAM_ADWAITA_DIR)
 			dev_reload(target)
 
 		print(f"{TEXT_GREEN}{TEXT_CHECK} Done!{TEXT_RESET}")
